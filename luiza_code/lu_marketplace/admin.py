cf9fa228db16e36e55d75935a73ebee0250c27e2
anageorgia <anageorgiagp@gmail.com>
2020-11-18 04:49:38 -0300

    Criando os models Vendedor e Produto

--- luiza_code/lu_marketplace/admin.py
+++ luiza_code/lu_marketplace/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from.models import Vendedor, Produto
 
-# Register your models here.
+admin.site.register(Vendedor)
+admin.site.register(Produto)
--- luiza_code/lu_marketplace/models.py
+++ luiza_code/lu_marketplace/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Vendedor(models.Model):
+    vend_nome = models.CharField(max_length=100, verbose_name='Nome')
+    vend_email = models.EmailField(max_length=254, verbose_name='E-mail')
+
+    def __str__(self) -> str:
+        return self.vend_nome
+
+    
+
+
+
+class Produto(models.Model):
+    prod_nome = models.CharField(max_length=100, verbose_name='Nome')
+    prod_descricao = models.TextField(max_length=100, verbose_name='Descrição')
+    prod_preco = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Preço')
+    prod_codigo = models.IntegerField(verbose_name='Código do Produto')
+    prod_qtd = models.IntegerField(verbose_name='Quantidade')
+    prod_inativo = models.BooleanField(default=False, verbose_name='Produto Inativo')
+    vend_id = models.ForeignKey(Vendedor, on_delete=CASCADE, verbose_name='Vendedor')
+
+    def __str__(self, vend_id) -> str:
+        return self.prod_nome
+    
