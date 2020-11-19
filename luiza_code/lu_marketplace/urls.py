c1123c81d5e8cb3ff551cfefc43d5d8f536ef006
suele <suelen.ads.guimaraes@gmail.com>
2020-11-19 02:49:25 -0300

    Adiciona a url da pagina index.html que eu tinha excluido no merge.

--- luiza_code/lu_marketplace/urls.py
+++ luiza_code/lu_marketplace/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
 from .views import CadastrarProduto
+from django.urls import path
+from.import views
 
 urlpatterns = [
+
+    path('', views.index, name='luMarketplace-index'),
 
     path('cad_produtos/', CadastrarProduto.as_view(),
          name='luMarketplace-cadastro')
