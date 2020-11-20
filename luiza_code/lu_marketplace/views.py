a92725b10e49daf9c3fb71d3e59a44b6d95f4bb2
suele <suelen.ads.guimaraes@gmail.com>
2020-11-20 00:58:53 -0300

    Adiciona a classe para buscar produto.

--- luiza_code/lu_marketplace/views.py
+++ luiza_code/lu_marketplace/views.py
@@ -5,7 +5,6 @@ from django.views.generic import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-
 def index(request):
     produtos = Produto.objects.all()
     search = request.GET.get('search')
@@ -16,22 +15,27 @@ def index(request):
     return render(request, 'lu_marketplace/index.html', context)
 
 
-
 class CadastrarProduto(SuccessMessageMixin, CreateView):
     model = Produto
     template_name = 'cadastrar_prod.html'
     fields = '__all__'
     success_message = 'Produto cadastrado com sucesso!'
-    def get_success_url(self):  
+
+    def get_success_url(self):
         return '/'
+
 
 class AtualizarProduto(SuccessMessageMixin, CreateView):
     model = Produto
     template_name = 'atualizar_prod.html'
     fields = '__all__'
     success_message = 'Produto atualizado com sucesso!'
-    def get_success_url(self):  
+
+    def get_success_url(self):
         return '/'
 
 
-
+class BuscarProduto(SuccessMessageMixin, CreateView):
+    model = Produto
+    template_name = 'buscar_produto.html'
+    fields = '__all__'
